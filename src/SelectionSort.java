b4102a71eecddd813a75a08f4def8b047691921c
Admin <sayaan@umich.edu>
2017-02-19 23:10:56 -0800

    2nd commit

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -38,20 +38,6 @@ public class SelectionSort {
         array[j] = temp; // orginal value of a[i]
     }
 
-    private static int[] getArray(){
-        Scanner keys =  new Scanner(System.in);
-        System.out.println("Please enter how many numbers you would like to be sorted would like sorted:");
-        int numberUsed = keys.next();
-        int[] temp = new int[numberUsed];
-
-        System.out.println("please enter the " + numberUsed + " as a list without commas to be sorted");
-        int index;
-        for (index = 0; index < numberUsed; index++){
-            temp[index] = keys.next();
-        }
-
-        return temp;
-    }
 
 
 }
